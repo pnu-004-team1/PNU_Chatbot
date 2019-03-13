e95f0c60c1955ffc57304071c215f5b4ef140fca
RYOO KANG HYUN <ryoogh96@gmail.com>
2019-03-13 20:38:34 +0900

    chatbot!

--- src/hello/helloworld.java
+++ src/hello/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("hello chatbot!");
 	}
 
 }
