af79826819786cc07414c266a48fcf479f267658
Kooku <koomg9599@gmail.com>
2019-04-06 21:10:32 +0900

    system.out.print 주석

--- src/hello/helloworld.java
+++ src/hello/helloworld.java
@@ -3,8 +3,8 @@ package hello;
 public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello chatbot!");
-        System.out.println("Bye chatbot");
-		System.out.println("Good bye");
+		// System.out.println("hello chatbot!");
+        // System.out.println("Bye chatbot");
+		// System.out.println("Good bye");
 	}
 }
