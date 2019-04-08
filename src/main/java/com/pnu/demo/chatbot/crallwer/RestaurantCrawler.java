3c6283fa1d3a97116c2f93bfaad06bf445d46997
starkim06 <starkim06@naver.com>
2019-04-09 01:06:29 +0900

    식당 및 식단 파싱

--- src/main/java/com/pnu/demo/chatbot/crallwer/RestaurantCrawler.java
+++ src/main/java/com/pnu/demo/chatbot/crallwer/RestaurantCrawler.java
@@ -9,40 +9,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestaurantCrawler {
     private final String RESTAURANT_URL = "http://www.pusan.ac.kr/kor/CMS/MenuMgr/menuListOnWeekly.do?mCode=MN203";
-    Document pnu;
+    private final String PATH = "=======================================================";
+    private final String[] MEAL = {"아침", "점심", "저녁"};
+    private Document pnu;
+    private int thisMeal = 0;
 
-    public void get(){
-        try{
+    public void get() {
+        try {
             pnu = Jsoup.connect(RESTAURANT_URL).get();
-            Elements elements = pnu.select("table tbody tr");
-            elements.forEach(element -> {
-                String name = element.select("th").text();
-                System.out.println(name);
-                Elements menus = element.select("td");
-                if(menus.text()==""|| menus.text()=="\n" || menus.text() == null){
-                    System.out.println("빈칸체크");
-                }
-                System.out.println(menus.html());
-//                menus.forEach(menu->{
-//                    Elements realMenus = menu.select("li");
-//                    realMenus.forEach(realMenu->{
-//                        System.out.println(realMenu.text());
-//                    });
-//                });
+            Elements restaurants = getRestaurant();
+            restaurants.forEach(restaurant -> {
+                thisMeal = 0;
+                String restaurantName = restaurant.select("th").text(); // 식당 이름
+                Elements restaurantMenus = restaurant.select("td");
+                System.out.println("식당 이름 : " + restaurantName);
+                restaurantMenus.forEach(restaurantMenu -> {
+                    System.out.println(MEAL[thisMeal]);
+                    if (restaurantMenu.text().equals("")) {
+                        System.out.println("X");
+                    } else {
+                        Elements eachMenu = restaurantMenu.select("ul li");
+                        eachMenu.forEach(element -> {
+                            System.out.println(element.text());
+                        });
+                    }
+                    thisMeal++;
+                    System.out.println();
+                });
+                System.out.println();
+                System.out.println();
             });
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
 
     }
 
-    public void getName(){
-        Elements elements = pnu.select("table tbody tr th");
-        elements.forEach(element -> {
-            System.out.println(element.text());
-        });
+    public Elements getRestaurant() {
+        Elements elements = pnu.select("table tbody tr");
+        return elements;
     }
-
 
 
 }
