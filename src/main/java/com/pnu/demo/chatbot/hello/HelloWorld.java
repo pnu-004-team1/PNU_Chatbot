ae24dac14bcf65012e998c1b0bc75a8a8c380d2f
RYOO KANG HYUN <ryoogh96@gmail.com>
2019-04-09 15:45:13 +0900

    이클립스 프로젝트를 IntellJ의 스프링 프로젝트로 이동 완료

--- src/main/java/com/pnu/demo/chatbot/hello/HelloWorld.java
+++ src/main/java/com/pnu/demo/chatbot/hello/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.pnu.demo.chatbot.hello;
 
 import com.pnu.demo.chatbot.crallwer.RestaurantCrawler;
+import com.pnu.demo.chatbot.libInfo.CentralLibCrawler;
+import com.pnu.demo.chatbot.libInfo.LibraryCrawler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -13,10 +15,10 @@ public class HelloWorld implements ApplicationRunner {
     private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
 
     @Autowired
-    RestaurantCrawler restaurantCrawler;
+    CentralLibCrawler centralLibCrawler;
     @Override
     public void run(ApplicationArguments args) throws Exception {
             // TODO Auto-generated method stub
-            restaurantCrawler.get();
+            centralLibCrawler.getResult();
     }
 }
---
+++ src/main/java/com/pnu/demo/chatbot/libInfo/LibraryCrawler.java
@@ -0,0 +1,32 @@
+package com.pnu.demo.chatbot.libInfo;
+
+public class LibraryCrawler {
+    private String url;
+    private String category;
+
+
+    public LibraryCrawler() {
+
+    }
+
+    public LibraryCrawler(String url) {
+        this.url = url;
+    }
+
+    private void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    private void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+}
