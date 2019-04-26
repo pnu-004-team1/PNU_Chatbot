84fb7842efe2f286f329ecf04dcaf0f22a19cce6
Doyoung Gwak <tucan.dev@gmail.com>
2019-04-27 01:30:01 +0900

    [서버 구축] 챗봇 메시지 API #PNUC-146

--- src/main/java/com/pnu/demo/chatbot/controller/SampleController.java
+++ src/main/java/com/pnu/demo/chatbot/controller/SampleController.java
@@ -1,13 +1,24 @@
 package com.pnu.demo.chatbot.controller;
 
+import com.pnu.demo.chatbot.service.ChatbotService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
+    private ChatbotService service = new ChatbotService();
+
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/chatbot")
+    public String chatbotmessage(@RequestParam String query) {
+        System.out.println(query);
+
+        return service.getAnswer(query);
     }
 }
