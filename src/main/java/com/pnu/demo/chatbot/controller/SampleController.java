7d4763ddc14572c59a61b19862b37ecc75f586db
Kooku <koomg9599@gmail.com>
2019-04-27 13:38:20 +0900

    크로스 도메인 문제 해결

--- src/main/java/com/pnu/demo/chatbot/controller/SampleController.java
+++ src/main/java/com/pnu/demo/chatbot/controller/SampleController.java
@@ -1,10 +1,12 @@
 package com.pnu.demo.chatbot.controller;
 
 import com.pnu.demo.chatbot.service.ChatbotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class SampleController {
 
