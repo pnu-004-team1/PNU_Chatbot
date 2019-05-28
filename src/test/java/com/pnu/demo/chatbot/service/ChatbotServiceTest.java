f758829ff25b20be85a8d5ead0385cf54180d6c9
Doyoung Gwak <tucan.dev@gmail.com>
2019-05-28 16:09:01 +0900

    [소프트웨어 품질 개선] 비지니스로직-도서관정보관리모듈 연결 테스트 케이스 작성 #PNUC-185

--- src/test/java/com/pnu/demo/chatbot/service/ChatbotServiceTest.java
+++ src/test/java/com/pnu/demo/chatbot/service/ChatbotServiceTest.java
@@ -1,5 +1,7 @@
 package com.pnu.demo.chatbot.service;
 
+import com.pnu.demo.chatbot.libInfo.LibraryInfoManager;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.logging.*;
@@ -9,36 +11,173 @@ public class ChatbotServiceTest {
 
     private static final Logger logger = Logger.getLogger(ChatbotServiceTest.class.getName());
 
-    @Test
-    public void test() {
-        ChatbotService service = new ChatbotService();
+    ChatbotService service;
 
-        String question;
-
-        question = "학사 일정 알려줘";
-        print(question, service.getAnswer(question));
-
-        question = "오늘 학식이 뭐야?";
-        print(question, service.getAnswer(question));
-
-        question = "중간고사 언제야?";
-        print(question, service.getAnswer(question));
-
-        question = "부산대 정보";
-        print(question, service.getAnswer(question));
-
-        question = "도서관 정보";
-        print(question, service.getAnswer(question));
-
-        question = "정보컴퓨터공학부 홈페이지";
-        print(question, service.getAnswer(question));
+    @Before
+    public void setUp() throws Exception {
+        service = new ChatbotService();
     }
 
-    private void print(String question, String answer) {
-        System.out.println("=====================================");
-        System.out.println("------------ Question ---------------");
-        System.out.println(question);
-        System.out.println("------------- Answer ----------------");
-        System.out.println(answer);
+    @Test
+    public void testLibraryInfoManager_jungang() {
+        String result;
+        String sut;
+
+        LibraryInfoManager infoManager = new LibraryInfoManager();
+
+        // "중앙도서관 학기중 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("중앙", "학기중");
+        // when
+        sut = service.getAnswer("중앙도서관 학기중 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+
+        // "중앙도서관 방학중 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("중앙", "방학중");
+        // when
+        sut = service.getAnswer("중앙도서관 방학중 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+
+//        // "중앙도서관 뭔가 정보 알려줘"
+//        // given
+//        result = infoManager.getStringResult("중앙", null);
+//        // when
+//        sut = service.getAnswer("중앙도서관 뭔가 정보 알려줘");
+//        // then
+//        assertEquals(sut, result);
+    }
+
+    @Test
+    public void testLibraryInfoManager_saebyuck() {
+        String result;
+        String sut;
+
+        LibraryInfoManager infoManager = new LibraryInfoManager();
+
+
+
+
+        // "새벽도서관 자료실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("새벽", "자료실");
+        // when
+        sut = service.getAnswer("새벽도서관 자료실 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+
+        // "새벽도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("새벽", "열람실");
+        // when
+        sut = service.getAnswer("새벽도서관 열람실 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+    }
+
+    @Test
+    public void testLibraryInfoManager_buphack() {
+        String result;
+        String sut;
+
+        LibraryInfoManager infoManager = new LibraryInfoManager();
+
+
+
+        // "법학도서관 자료실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("법학", "자료실");
+        // when
+        sut = service.getAnswer("법학도서관 자료실 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+        // "법학도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("법학", "열람실");
+        // when
+        sut = service.getAnswer("법학도서관 열람실 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+    }
+
+    @Test
+    public void testLibraryInfoManager_mirinae() {
+        String result;
+        String sut;
+
+        LibraryInfoManager infoManager = new LibraryInfoManager();
+
+
+        // "미리내도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("미리내", "평소");
+        // when
+        sut = service.getAnswer("미리내도서관 평소 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+        // "미리내도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("미리내", "시험기간");
+        // when
+        sut = service.getAnswer("미리내도서관 시험기간 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+    }
+
+    @Test
+    public void testLibraryInfoManager_sangmyeong() {
+        String result;
+        String sut;
+
+        LibraryInfoManager infoManager = new LibraryInfoManager();
+
+
+        // "의생명도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("의생명", "학기중");
+        // when
+        sut = service.getAnswer("의생명도서관 학기중 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+        // "의생명도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("의생명", "방학중");
+        // when
+        sut = service.getAnswer("의생명도서관 방학중 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+    }
+
+    @Test
+    public void testLibraryInfoManager_nano() {
+        String result;
+        String sut;
+
+        LibraryInfoManager infoManager = new LibraryInfoManager();
+
+
+        // "나노생명도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("나노생명", "자료실");
+        // when
+        sut = service.getAnswer("나노생명도서관 자료실 정보 알려줘");
+        // then
+        assertEquals(sut, result);
+
+        // "나노생명도서관 열람실 정보 알려줘"
+        // given
+        result = infoManager.getStringResult("나노생명", "열람실");
+        // when
+        sut = service.getAnswer("나노생명도서관 열람실 정보 알려줘");
+        // then
+        assertEquals(sut, result);
     }
 }
