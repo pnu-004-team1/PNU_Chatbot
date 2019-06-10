773970cdc4834fe2332df29aacd11ab4034f7f65
Kooku <koomg9599@gmail.com>
2019-06-11 00:07:05 +0900

    bookinfo 개행 삽입

--- src/main/java/com/pnu/demo/chatbot/bookInfo/BookInfoManager.java
+++ src/main/java/com/pnu/demo/chatbot/bookInfo/BookInfoManager.java
@@ -10,6 +10,7 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class BookInfoManager {
@@ -19,15 +20,16 @@ public class BookInfoManager {
         String result = null;
         BookInfoManager http = new BookInfoManager();
         try {
-            result = http.sendGet("https://lib.pusan.ac.kr/resource/?query="+title).toString();
+            result = http.sendGet("https://lib.pusan.ac.kr/resource/?query="+title);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
     }
     // HTTP GET request
-    private JSONArray sendGet(String targetUrl) throws Exception {
-        JSONArray book_array = new JSONArray();
+    private String sendGet(String targetUrl) throws Exception {
+//        JSONArray book_array = new JSONArray();
+        String result = "";
         try {
 
             // 1. HTML 가져오기
@@ -49,30 +51,46 @@ public class BookInfoManager {
                 String title = e.select(".title").text();
                 String author = e.select("dd.author").text();
                 String publication = e.select("dd.publication").text();
+                result += (
+                        "제목: " + title + "\n" +
+                        "저자: " + author + "\n" +
+                        "출판사: " + publication + "\n"
+                );
                 JSONArray bookStatusArray = new JSONArray();
                 Element rd_list = e.select(".rd-list.inline.location").get(0);
+                ArrayList<String> locations = new ArrayList<>();
                 for (Element rd : rd_list.select("dt")) {
                     JSONObject bookLocation = new JSONObject();
                     bookLocation.put("location", rd.text());
                     bookStatusArray.add(bookLocation);
+                    locations.add(rd.text());
                 }
+                ArrayList<String> status = new ArrayList<>();
                 Elements rd_status = rd_list.select("dd span");
                 for (int index = 0; index < rd_status.size(); index++) {
                     JSONObject bookStatus = (JSONObject) bookStatusArray.get(index);
                     bookStatus.put("status", rd_status.get(index).text().replace("[", "").replace(" ]", ""));
+                    status.add(rd_status.get(index).text().replace("[", "").replace(" ]", ""));
                 }
+                String bookStatus = "";
+                for (int index = 0; index < locations.size(); index++) {
+                    bookStatus += "[" + locations.get(index) + "] " + status.get(index) + "\n";
+                }
+
+                result += "대여 상태: " + "\n" + bookStatus + "\n";
                 JSONObject bookObject = new JSONObject();
                 bookObject.put("thumbnail", thumbnail);
                 bookObject.put("title", title);
                 bookObject.put("author", author);
                 bookObject.put("publication", publication);
                 bookObject.put("status_list", bookStatusArray);
-                book_array.add(bookObject);
+//                book_array.add(bookObject);
             }
         } catch (IOException e) {
             // Exp : Connection Fail
             e.printStackTrace();
         }
-        return book_array;
+//        return book_array;
+        return result;
     }
 }
