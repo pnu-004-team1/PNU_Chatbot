f74d6427b630d2b96d5cab1b6204073a71a27936
Doyoung Gwak <tucan.dev@gmail.com>
2019-06-10 23:24:47 +0900

    [소프트웨어 품질 개선] 전체 코드 커버리지 개선 - 자연어처리 테스트케이스 추가

--- src/test/java/com/pnu/demo/chatbot/nlp/TextClassifierTest.java
+++ src/test/java/com/pnu/demo/chatbot/nlp/TextClassifierTest.java
@@ -1,39 +1,138 @@
 package com.pnu.demo.chatbot.nlp;
 
+import com.pnu.demo.chatbot.service.ChatbotServiceDelegate;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class TextClassifierTest {
+public class TextClassifierTest implements ChatbotServiceDelegate {
+
+    TextClassifier classifier;
 
     @Before
     public void setUp() throws Exception {
+        classifier = new TextClassifier();
     }
 
     @Test
     public void test() {
-        TextClassifier classifier = new TextClassifier();
+        String expectedValue;
+        String query = "";
+        ClassificationResult sut;
 
-//        String text = "오늘 학식이 뭐야?";
-//        String result = classifier.classify(text);
-//        System.out.println(result);
-//
-//        text = "도서관 열람실 자리 얼마나 남았어?";
-//        String result = classifier.classify(text);
-//        System.out.println(result);
-//
-//        text = "학사일정 알려줘";
-//        String result = classifier.classify(text);
-//        System.out.println(result);
-//
-//        text = "장학금 종류는?";
-//        String result = classifier.classify(text);
-//        System.out.println(result);
-//
-//        text = "금정식당 메뉴가 뭐야?";
-//        String result = classifier.classify(text);
-//        System.out.println(result);
+        // given
+        expectedValue = "getEArtical";
+        query = "논문";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getLibrarySeatingInfo";
+        query = "열람실좌석";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getUniversityCalendar";
+        query = "학사일정";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getLibraryContactInfo";
+        query = "도서관 관리자";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getLibraryContactInfo";
+        query = "도서관 관리자";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
     }
 
+    @Test
+    public void libraryInfo() {
+        String expectedValue;
+        String query = "";
+        ClassificationResult sut;
+
+        // given
+        expectedValue = "getLibraryInfo";
+        query = "도서관";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getLibraryInfo";
+        query = "학기중 중앙도서관";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getLibraryInfo";
+        query = "방학중 중앙도서관";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+
+        // given
+        expectedValue = "getLibraryInfo";
+        query = "그냥 중앙도서관";
+        // when
+        sut = classifier.classify(this, query);
+        // then
+        assertEquals(expectedValue, sut.answer);
+    }
+
+    @Override
+    public String getEArtical(String query) {
+        return "getEArtical";
+    }
+
+    @Override
+    public String getLibraryInfo(String libName, String category) {
+        return "getLibraryInfo";
+    }
+
+    @Override
+    public String getLibrarySeatingInfo(String query) {
+        return "getLibrarySeatingInfo";
+    }
+
+    @Override
+    public String getLibraryContactInfo(String libName) {
+        return "getLibraryContactInfo";
+    }
+
+    @Override
+    public String getUniversityCalendar() {
+        return "getUniversityCalendar";
+    }
+
+    @Override
+    public String getFoodCafeInfo(String query) {
+        return "getFoodCafeInfo";
+    }
+
+    @Override
+    public String getExceptionMessage() {
+        return "getExceptionMessage";
+    }
 }
